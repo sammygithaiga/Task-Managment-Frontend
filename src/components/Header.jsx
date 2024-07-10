d38ad29e54ae47e0a744b754246dc0066b739339
collins kipngeno <kipngenolins01@gmail.com>
2024-07-10 10:26:04 +0300

    Initialcommit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Task Management App</h1>
+      <div className="profile">
+        <span>User Profile</span>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
