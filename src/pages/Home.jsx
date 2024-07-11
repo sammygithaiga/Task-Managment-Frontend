f0580805513de3623a38ad87cd8ffc934a9dc885
collins kipngeno <kipngenolins01@gmail.com>
2024-07-11 09:19:22 +0300

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Home = () => {
     return (
         <div>
-            <h1>Welcome to the Task Management App</h1>
+            <h1>Welcome To The Task Management App</h1>
         </div>
     );
 };
